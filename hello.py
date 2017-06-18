90a43ebe7371c8b57b7957619e6e0089ad8b511d
ray-python <QHXV09IMgit>
2017-06-18 19:25:12 +0300

    test

--- hello.py
+++ hello.py
@@ -1,5 +1,4 @@
 def wsgi_application(environ, start_response):
-    # бизнес-логика
     status = '200 OK'
     headers = [
         ('Content-Type', 'text/plain')
@@ -7,3 +6,24 @@ def wsgi_application(environ, start_response):
     body = 'Hello, world!'
     start_response(status, headers )
     return [body]
+
+'''
+def app(environ, start_response):
+
+	
+	raw_uri = str(environ.get('RAW_URI'))
+	
+	raw_uri = raw_uri[2:]
+
+	params = raw_uri.split('&')
+	
+	data = ''
+	for param in params:
+		data += param + '\r\n'
+	
+	start_response("200 OK", [
+	  ("Content-Type", "text/plain"),
+	  ("Content-Length", str(len(data)))
+	])
+	return iter([data])
+	'''
