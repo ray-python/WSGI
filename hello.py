802ae0b8af0f6cd9483c2698fa7059d2c9757334
ray-python <QHXV09IMgit>
2017-06-18 20:45:59 +0300

    test

--- hello.py
+++ hello.py
@@ -3,11 +3,6 @@ def app(environ, start_response):
     headers = [
         ('Content-Type', 'text/plain')
     ]
-    #body = 'Hello, world!'
     body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
     start_response(status, headers )
     return body
-
-'''
-	body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
-	'''
